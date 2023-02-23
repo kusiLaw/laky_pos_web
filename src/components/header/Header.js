1032f51f688ddc6e7f49e0d590ab1b8fa149ab3e
kusi <lawcubegsm@gmail.com>
2023-02-23 12:28:37 +0000

    Add page css setting

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+      <div className='header_1-wrapper'>
+        <di className='header_1_inner_wrapper'>
+
+        </di>
+      </div>
+
+      <div className='header-div-2'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
