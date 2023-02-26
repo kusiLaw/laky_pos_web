1b9e2807d092f091c633612db231e1629a1d0538
kusi <lawcubegsm@gmail.com>
2023-02-26 15:58:55 +0000

    add presentation holders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
 import Header from './components/header/Header';
 
+
+
 function App() {
 
-  const s = (num) =>{
-  let l = []
-   let i = 0
-   while(i < num){
-    l.push('gjggigi')
-    i += 1
-   }
-   return l
-  }
   return (
     <div className='container'>
      <Header />
-     {
-      s(80).map(el => <div>el</div>)
-     }
+     <main className='main'>
+        <section className='presentation' >
+          <div className='features flex'>
+
+             <div className='features_text'>
+                <h1>LAKY POS FOR SMALL AND LARGE BUSINESSES</h1>
+                <p className=''>
+                   Fast, Scalable, and Results Driven Email API's Built for Developers
+                </p>
+             </div>
+
+             <div className='features_img'>
+                 <img src={require(`./asserts/images/Capture.PNG`)} alt = '' className='app_photo left'/>
+                 {/* <img src={require(`./asserts/images/Capture5.PNG`)} alt = '' className='app_photo right'/> */}
+                 <img src={require(`./asserts/images/Capture2.PNG`)} alt = '' className='app_photo center'/>
+                 {/* <img src={require(`./asserts/images/Capture3.PNG`)} alt = '' className='app_photo b-right'/>
+                 <img src={require(`./asserts/images/Capture4.PNG`)} alt = '' className='app_photo b-left'/> */}
+
+             </div>
+
+          </div>
+        </section>
+     </main>
+     
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,19 +4,19 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header_wrapper c-flex'>
+    <header className='header_wrapper c-flex'>
       <div className='header_1_wrapper flex'>
         <div className='header_1_inner_wrapper flex'>
 
         </div>
       </div>
-
+      
       <div className='header_2_wrapper flex'>
         <div className='header_2_inner_wrapper flex'>
 
          </div>
       </div>
-    </div>
+    </header>
   )
 }
 
